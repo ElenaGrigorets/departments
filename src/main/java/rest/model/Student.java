6efa6c031934b278a0849a4f6566e8da9108c170
helen <adamenkolena7@gmail.com>
2018-11-25 16:59:33 +0200

    rest

---
+++ src/main/java/rest/model/Student.java
@@ -0,0 +1,26 @@
+package rest.model;
+
+/**
+ * Created by dik81 on 25.11.18.
+ */
+
+public class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
